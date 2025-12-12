021bd89873bef6c0d4a6d9a97a489f802f19a648
goodsy <goodsy@gmail.com>
2025-12-12 13:18:09 +0900

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://goodsy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sykim-project/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'goodsy', // Usually your GitHub org/user name.
+  projectName: 'sykim-project', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
