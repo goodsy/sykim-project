88e246acf45e84d356f618efc2f782ea71ba3450
goodsy <goodsy@gmail.com>
2025-12-12 14:39:19 +0900

    Deploy website - based on 021bd89873bef6c0d4a6d9a97a489f802f19a648

---
+++ assets/js/64707dbd.4368a371.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksykim_project=globalThis.webpackChunksykim_project||[]).push([[1599],{8440:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/sykim-project/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
